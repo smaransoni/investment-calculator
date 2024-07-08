5863692a39fc0aca0f6644d63e62cf928c860b4d
Smaran Soni <smaransoni@gmail.com>
2024-07-08 19:21:02 +0100

    fix: Update App.jsx to use parseInt() when receiving user input values from UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -14,16 +15,21 @@ function App() {
     setUserInput(prevUserInput => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue, // Square brackets allow you to use a variable or expression to dynamically access a property name within an object.
+        [inputIdentifier]: parseInt(newValue), // Square brackets allow you to use a variable or expression to dynamically access a property name within an object.
+        // type casting the string value received from the event.target.value (by default provides a string) in the UserInput.jsx
       };
     });
-    console.log(userInput); // consoling it here displays the previous userInput value and not the updated value. Confirming that the state update is not instant in React.
+    // console.log(userInput); // consoling it here displays the previous userInput value and not the updated value. Confirming that the state update is not instant in React.
   };
 
   return (
     <>
       <Header>React Investment Calculator</Header>
-      <UserInput userInput onInput={userInputHandler}></UserInput>
+      <UserInput
+        input={userInput} //input prop provided with the userInput object
+        onChangeInput={userInputHandler}
+      ></UserInput>
+      <Results input={userInput}></Results>
     </>
   );
 }
