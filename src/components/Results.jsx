df7b438de20bbb546b1bdbeb5d6563292edf04c6
Smaran Soni <smaransoni@gmail.com>
2024-07-08 19:23:15 +0100

    feat: Add Results.jsx for displaying the tabulated values

---
+++ src/components/Results.jsx
@@ -0,0 +1,51 @@
+import { calculateInvestmentResults, formatter } from '../util/investment';
+const Results = function ({ input }) {
+  //   console.log('userInput:', { ...input });
+
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map(yearData => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+
+          const totalAmountInvested =
+            initialInvestment + yearData.annualInvestment * yearData.year;
+          //  const totalAmountInvested =
+          //    yearData.valueEndOfYear - totalInterest;
+
+          return (
+            // Since, we are in a list, the table row should receive a key
+            // yearData.year is suitable as a key as it has a different value for every year
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Results;
